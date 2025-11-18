5bec13192f935931a91140b638c331d21071b85f
azharf99 <azharfaturohman29@gmail.com>
2025-11-18 17:42:19 +0700

    Add student report template with detailed information and feedback

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -5,7 +5,10 @@ from groups.models import Group
 class Feedback(models.Model):
     group = models.ForeignKey(Group, on_delete=models.SET_NULL, related_name='feedbacks', null=True)
     number = models.PositiveBigIntegerField()
-    message = models.TextField()
+    topic = models.CharField(max_length=100, blank=True, null=True)
+    result = models.TextField(blank=True, null=True)
+    competency = models.TextField(blank=True, null=True)
+    tutor_feedback = models.TextField(blank=True, null=True)
     is_sent = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- feedbacks/views.py
+++ feedbacks/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from feedbacks.models import Feedback
 from feedbacks.serializers import FeedbackSerializer
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from utils.pagination import StandardResultsSetPagination
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.template.loader import render_to_string
+from django.http import HttpResponse, HttpResponseRedirect
+from weasyprint import HTML
 from utils.whatsapp import create_schedule
 # Create your views here.
 
@@ -16,15 +20,43 @@ class FeedbackViewSet(viewsets.ModelViewSet):
         AnonRateThrottle,
     ]
 
-    def perform_create(self, serializer):
-        feedback = serializer.save()
-        data = {
-            'phone': '6281218xxxxxx',
-            'date': '2022-05-20',
-            'time': '13:20:00',
-            'timezone': 'Asia/Jakarta',
-            'message': 'hello',
-            'isGroup': 'true',
-        }
-        create_schedule(data)
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        for feedback in queryset:
+            print(feedback.group.lessons.filter(title__icontains='Python'))
+            for student in feedback.group.students.all():
+                print(student.phone_number)
 
+        return super().list(request, *args, **kwargs)
+
+    # def perform_create(self, serializer):
+    #     feedback = serializer.save()
+    #     phone_numbers = []
+    #     for student in feedback.group.students.all():
+    #             phone_numbers.append(student.parent_contact)
+
+    #     data = {
+    #         'phone': '6281218xxxxxx',
+    #         'date': '2022-05-20',
+    #         'time': '13:20:00',
+    #         'timezone': 'Asia/Jakarta',
+    #         'message': 'hello',
+    #         'isGroup': 'true',
+    #     }
+    #     create_schedule(data)
+
+
+
+class FeedbackDownloadPDFView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    raise_exception = False
+    permission_required = 'feedbacks.view_feedback'
+    queryset = Feedback.objects.select_related("group").all()
+
+    def get(self, request, *args, **kwargs):
+        html = render_to_string('index.html', {
+            'data': "Aku",
+        })
+        pdf = HTML(string=html).write_pdf()
+        response = HttpResponse(pdf, content_type='application/pdf')
+        response['Content-Disposition'] = f'attachment; filename="Feedbacks.pdf"'
+        return response
